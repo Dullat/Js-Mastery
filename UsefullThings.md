8928b0431ffd3bea0d7072a4d50eb018aa407ff7
Dullat <142017979+Dullat@users.noreply.github.com>
2025-06-10 16:30:04 +0530

    Funs deeply: declaration, expression, named, anoany, first-class

---
+++ UsefullThings.md
@@ -0,0 +1,40 @@
+# JavaScript
+
+## Advance Concepts
+#### Call stack
+1. https://www.javascripttutorial.net/javascript-call-stack/
+Event Loop
+2. https://youtu.be/8aGhZQkoFbQ?si=blXa2kk12yNlwvbp
+3. effective : https://youtu.be/8zKuNo4ay8E?si=nozvklqpk8jVKaKM
+**Promisis**
+4. https://youtu.be/2d7s3spWAzo?si=w8y2-dFld6OPentc
+
+**Objects**
+1. **Protos** https://youtu.be/YkoelSTUy7A?si=m177YcdX47Y9CCVQ
+
+
+
+---
+higher order funs
+
+call backs: https://youtu.be/btj35dh3_U8?si=0tUPvqrFJ_78m7hd
+
+this : https://youtu.be/9ksqBa8_txM?si=fS4AiBZ1Q7OmIu8G
+
+---
+# CSS and Canvas
+
+## Learning Games
+
+1. **Flex-box Froggy** https://flexboxfroggy.com/
+2. **Grid-Gardin** https://cssgridgarden.com/
+3. **Array and Other things** https://codepip.com/games/disarray/
+4. **CSS Tricks** https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+## Themes
+
+1. Trending Colors https://coolors.co/palettes/trending
+
+## Videos
+
+1. Flex-box Crash course https://youtu.be/xN1BkNItTRk?si=Iwf1Byg0E1A8VheI
--- code/8.functions2.js
+++ code/8.functions2.js
@@ -1,7 +1,87 @@
 // greetFun() this wont work , sure vars are hoisted but not fun bodies so it will say fun not defined
 
-var greetFun = function greet(){
+var greetFun = function greet() {
     console.log("im greeting");
 }
 
 console.log(greetFun.toString());
+
+//############# Real Shit #############
+
+
+// function statement aka fun declaration
+
+function a() {
+    consolel.log("that's it, this is a fun statement")
+    // will be hoisted
+}
+
+// function expresstion
+
+var b = function (value) {
+    console.log("assigning fun to a variable") // The main diff between statement and expression is that statement fun is hoisted but in expression its not 
+    // but if var then the var will be hoisted not the fun body and let and const are not hoisted at all
+}
+
+
+
+// function declaration
+
+function vb() {
+    // its nothig just same , decleration and statement are same
+    // it is used only the places where funs are used as values
+}
+
+
+
+// Anonymous Function
+
+function () {
+    // they dont have identity, also this will give error and invalid syntax
+}
+
+let anony = function () { }
+
+
+
+
+// Named function Expression
+
+var namedFun = function xyz() {
+    xyz()
+} // if you try xyz() this will give error , coz you created it as value or local scop to varibale
+// but you can call it inside itself , very usefull in recursion
+
+
+
+// Diff in Perameters and Args
+
+function diffs(para1, pera2) { }
+
+diffs(arg1, arg1)
+
+
+
+
+// First class functions or First class citizen
+
+// basically passing funs as args, returning funs, or treating them as values
+
+function castMsg(fn) {
+    fn()
+    return function () {
+        return "im returning"
+    }
+}
+
+const greet = function () {
+    console.log("im greeting");
+}
+
+let gotSomethingHere = castMsg(greet)
+gotSomethingHere = castMsg(function () { console.log("im first class too") })
+
+
+// Arrow functions
+
+// will cover independently , deeply
