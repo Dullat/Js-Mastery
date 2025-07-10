4bc9ed2a0ffcbf0c6823f9cdf124c27c0e99e4c3
Dullat <142017979+Dullat@users.noreply.github.com>
2025-07-10 18:41:02 +0530

    load products form backend

--- projects/amazon-project/scripts/checkout.js
+++ projects/amazon-project/scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { paymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 // import '../data/cart.class.js'
-import '../data/backend-practice.js'
+// import '../data/backend-practice.js'
 
-renderOrderSummary()
-paymentSummary()
+loadProducts(() => {
+    renderOrderSummary()
+    paymentSummary()
+})
