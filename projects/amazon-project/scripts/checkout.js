15c8a14f7eb2a624875842a417c53a1e4fb7a70d
Dullat <142017979+Dullat@users.noreply.github.com>
2025-07-10 22:10:27 +0530

    used fetch in checkout "loadProdustsFetch"

--- projects/amazon-project/scripts/checkout.js
+++ projects/amazon-project/scripts/checkout.js
@@ -1,19 +1,24 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { paymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 // import '../data/cart.class.js'
 // import '../data/backend-practice.js'
 
 
-new Promise((resolve) => { // this thing's md is in theory
-    loadProducts(() => {
-        renderOrderSummary()
-        paymentSummary()
-        resolve();
-    })
-}).then(()=> {
-    console.log("finished loading and rendring")
+loadProductsFetch().then(() => {
+    renderOrderSummary()
+    paymentSummary()
 })
+
+// new Promise((resolve) => { // this thing's md is in theory
+//     loadProducts(() => {
+//         renderOrderSummary()
+//         paymentSummary()
+//         resolve();
+//     })
+// }).then(()=> {
+//     console.log("finished loading and rendring")
+// })
 
 // loadProducts(() => {
 //     renderOrderSummary()
